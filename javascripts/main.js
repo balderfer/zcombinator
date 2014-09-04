94a0e0825cc5f8736a51c707b4ea719842cf9d62
Ben Alderfer <balderfe@purdue.edu>
2014-09-04 02:11:59 -0400

    Sweet animationion

--- javascripts/main.js
+++ javascripts/main.js
@@ -41,9 +41,37 @@ jQuery(document).ready(function($) {
             margin: "60px auto 20px auto",
         }, 500);
         $(".container").fadeIn(1000, function() {
-            
+            $(window).scroll(function(event) {
+                var count = 0;
+                var newclass;
+                var element;
+                $(".pic").each(function(index, el) {
+                    if (index > count) {
+                        count = index;
+                    }
+                    newclass = "pic" + index;
+                    $(el).addClass(newclass);
+                });
+                for (var i = 0; i <= count; i++) {
+                    element = ".pic" + i;
+                    var position = $(element).offset();
+                    var posTop = position.top - $(window).scrollTop() + $(element).height() + 100;
+                    if (i <= 1) {
+                        if (posTop < $(window).height()) {
+                            $(element).delay(i * 100).animate({opacity: 1}, 500);
+                            $(element).parent().children('p').animate({opacity: 1}, 500);
+                        }
+                    }
+                    else if (i <= 4) {
+                        if (posTop < $(window).height()) {
+                            $(element).delay((i - 2) * 100).animate({opacity: 1}, 500);
+                            $(element).parent().children('p').animate({opacity: 1}, 500);
+                        }
+                    }
+                }
+            });
         });
-    }, 1000)
+    }, 1000);
 
 });
 
